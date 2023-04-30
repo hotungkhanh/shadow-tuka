fc88f3167bbb4059843ed61a0fbca2cb10c66a29
tungkhanhh <tungkhanh.ho@student.unimelb.edu.au>
2023-04-30 22:41:21 +1000

    Pellet class extends StationaryObject

--- src/Pellet.java
+++ src/Pellet.java
@@ -1,27 +1,9 @@
 import bagel.Image;
 import bagel.util.Point;
-import bagel.util.Rectangle;
 
-public class Pellet {
+public class Pellet extends StationaryObject {
     private final static Image PELLET_IMAGE = new Image("res/pellet.png");
-    private final Rectangle pelletRectangle;
     public Pellet(Point topLeft) {
-        pelletRectangle = new Rectangle(topLeft, PELLET_IMAGE.getWidth(), PELLET_IMAGE.getHeight());
-    }
-
-    /**
-     * Checks if the player after moving
-     * collides with the dot
-     */
-    public boolean collidesWith(Player player) {
-        return player.getPlayerGo().intersects(this.pelletRectangle);
-    }
-
-    /**
-     * If the dot is not eaten, draws the dot image
-     * at the coordinate of the dot Rectangle
-     */
-    public void draw() {
-        PELLET_IMAGE.drawFromTopLeft(pelletRectangle.left(), pelletRectangle.top());
+        super(PELLET_IMAGE, topLeft);
     }
 }
