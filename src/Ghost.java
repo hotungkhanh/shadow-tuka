6aacbb3946479372a84fbc80a706a3a4a0436346
tungkhanhh <tungkhanh.ho@student.unimelb.edu.au>
2023-05-17 13:56:32 +1000

    Add eyes to Ghosts

--- src/Ghost.java
+++ src/Ghost.java
@@ -5,6 +5,10 @@ import java.util.ArrayList;
 
 public abstract class Ghost extends MovingEntity {
     private final static Image GHOST_FRENZY_IMAGE = new Image("res/ghostFrenzy.png");
+    private final static Image EYES_LEFT_IMAGE = new Image("res/eyesLeft.png");
+    private final static Image EYES_RIGHT_IMAGE = new Image("res/eyesRight.png");
+    private final static Image EYES_UP_IMAGE = new Image("res/eyesUp.png");
+    private final static Image EYES_DOWN_IMAGE = new Image("res/eyesDown.png");
     public final static int FRENZY_SCORE = 30;
     public final static int DOWN = 0;
     public final static int RIGHT = 1;
@@ -32,6 +36,15 @@ public abstract class Ghost extends MovingEntity {
                     GHOST_FRENZY_IMAGE.drawFromTopLeft(getPosition().x, getPosition().y);
                 } else {
                     update();
+                    if (direction == LEFT) {
+                        EYES_LEFT_IMAGE.drawFromTopLeft(getPosition().x, getPosition().y);
+                    } else if (direction == RIGHT) {
+                        EYES_RIGHT_IMAGE.drawFromTopLeft(getPosition().x, getPosition().y);
+                    } else if (direction == UP) {
+                        EYES_UP_IMAGE.drawFromTopLeft(getPosition().x, getPosition().y);
+                    } else {
+                        EYES_DOWN_IMAGE.drawFromTopLeft(getPosition().x, getPosition().y);
+                    }
                 }
             }
         }
